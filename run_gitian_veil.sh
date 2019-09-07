ddd22b5c5aec93e5bce797007382f884d52ed0f4
Veil-Project <45841695+Veil-Project@users.noreply.github.com>
2019-09-07 01:56:54 -0700

    Update for Veil

--- run_gitian_veil.sh
+++ run_gitian_veil.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 THISDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-docker run -h builder --name builder \
+docker run -h gitian_veil --name gitian_veil \
 -v $THISDIR/cache:/shared/cache:Z \
 -v $THISDIR/result:/shared/result:Z \
-builder
+gitian_veil
